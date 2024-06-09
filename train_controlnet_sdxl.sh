53c6ae59c872d9f2a9948e45259e4dd52f33362b
hey1derland <hyewon6385@gmail.com>
2024-06-09 15:27:24 +0900

    update

--- train_controlnet_sdxl.sh
+++ train_controlnet_sdxl.sh
@@ -6,14 +6,17 @@ export OUTPUT_DIR="/home/heywon/cnrl/save"
 accelerate launch --config_file ./ac_config.yaml /home/heywon/cnrl2/train_controlnet_sdxl.py \
  --pretrained_model_name_or_path=$MODEL_DIR \
  --output_dir=$OUTPUT_DIR \
- --resolution=512 \
- --train_data_dir='/scratch/heywon/data/output.jsonl' \
+ --resolution=1024 \
+ --train_data_dir='/scratch/heywon/data/output1.jsonl' \
  --train_batch_size=1 \
- --gradient_accumulation_steps=4 \
+ --gradient_accumulation_steps=1 \
  --gradient_checkpointing \
  --enable_xformers_memory_efficient_attention \
  --set_grads_to_none \
  --mixed_precision='fp16' \
  --image_column='image'\
  --caption_column='text' \
- --conditioning_image_column='condition_image'
+ --conditioning_image_column='condition_image' \
+ --use_8bit_adam \
+ --pretrained_vae_model_name_or_path=madebyollin/sdxl-vae-fp16-fix \
+ --controlnet_model_name_or_path=thibaud/controlnet-openpose-sdxl-1.0
